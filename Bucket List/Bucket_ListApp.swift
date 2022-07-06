99d765e816fb77d37247cb83150b1aeca0dc294c
Takasur Azeem <takasurazeem@gmail.com>
2022-07-06 12:02:07 +0500

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Takasur Azeem on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucket List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bucket List
+//
+//  Created by Takasur Azeem on 06/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
