8bcf2761b11530a25921a33749d0bab2464ae652
Takasur Azeem <takasurazeem@gmail.com>
2022-07-13 18:05:47 +0500

    Downloading data from Wikipedia
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Bucket List/Learning/MapKit/EditView.swift
+++ Bucket List/Learning/MapKit/EditView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct EditView: View {
+    
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
     @Environment(\.dismiss) var dismiss
     var location: Location
 
     @State private var name: String
     @State private var description: String
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
 
     var onSave: (Location) -> Void
     
@@ -31,6 +38,23 @@ struct EditView: View {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
                 }
+                
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loading:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                    case .loaded:
+                        Text("Loading…")
+                    case .failed:
+                        Text("Please try again later.")
+                    }
+                }
             }
             .navigationTitle("Place details")
             .toolbar {
@@ -45,6 +69,31 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
+        }
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            // success - convert the array values to our pages array
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
         }
     }
 }
@@ -55,4 +104,19 @@ struct EditView_Previews: PreviewProvider {
             // do nothing
         }
     }
+}
+
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
 }
